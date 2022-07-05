dc9f30eed183093e9e0f90baad8d06abd8aa7b21
darrenhke <91587264+darrenhke@users.noreply.github.com>
2022-07-05 23:20:56 +0800

    Routing from src to destination has been completed

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const path = require('path');
 const csv = require('fast-csv');
 
 //TODO: Create Djkstra algorithm and route person without startDateTime factors
-//TODO: Fidn route from start to end destination
+//TODO: Find route from start to end destination
 //TODO: Add in time factor
-//TODO: Remove user from 
 
 const trainMap = new Map()
 const interMap = new Map()
 const trainCodeList = []
 const trainName = []
+const parents = new Array(166)
+const pathListIndex = []
 
 let dist = new Array()
-var interChangeIndex = 0
 var currentVertices = 0 
 
 function readCSV(){
@@ -28,8 +28,8 @@ function readCSV(){
                 currentValue = trainMap.get(row.stationName)
                 trainMap.set(row.stationName, currentValue.concat(row.stationCode))
                 interMap.set(row.stationName, currentValue.concat(row.stationCode))
-                console.log(interChangeIndex+' ' + row.stationName,' is an interchange connected via ',trainMap.get(row.stationName))
-                interChangeIndex++
+                // console.log(interChangeIndex+' ' + row.stationName,' is an interchange connected via ',trainMap.get(row.stationName))
+                //Count the number of interchanges in Singapore
 
          }
         else{
@@ -41,7 +41,7 @@ function readCSV(){
         console.log(`Total amount of train stations amount to ${rowCount}`)
         console.log(`Total amount of interchanges amount to ${interMap.size}`)
         currentVertices = rowCount
-        console.log(trainCodeList)
+        // console.log(trainCodeList)
         resolve(rowCount)
     });
     }
@@ -49,24 +49,22 @@ function readCSV(){
 
 // Helper Function Create adjacency matrix
  function createAdjMatrix(rows,columns){
-    console.log('Entry into createAdjMatrix func')
+    // console.log('Entry into createAdjMatrix func')
     //Create 2D array
     const zeros = Array.from(Array(rows), _ => Array(columns).fill(0));
     for(let i =0; i < rows; i++){
         var currentVerticeTrainCode = trainCodeList[i].slice(0,2) 
-        console.log(i)
+        // console.log(i)
         for(let j =0; j < columns; j++){
             //Check if there is adjacent train station in the previous and exist
             if(trainCodeList[j-1]){
                 var previousVertice = trainCodeList[j-1]
                 var previousVerticeTrainCode = previousVertice.slice(0,2)
-                var previousVerticeTrainNumber = previousVertice.slice(2,previousVertice.length)
             }
 
             if(trainCodeList[j+1]){
                 var nextVertice = trainCodeList[j+1]
                 var nextVerticeTrainCode = nextVertice.slice(0,2)
-                var nextVerticeTrainNumber = nextVertice.slice(2,nextVertice.length)
             }
             // Check if previous station alphabet code is the same AND previous one is a number and the current vertice is being worked on
             if(currentVerticeTrainCode === previousVerticeTrainCode && i===j){
@@ -81,12 +79,12 @@ function readCSV(){
             //Check if this is the current vertice and check if the current train station is a interchange
             if(i===j && interMap.has(trainName[j])){
                 interchangeTrainCodes = Object.values(interMap.get(trainName[j])).filter(value => value != trainCodeList[j])
-                  console.log('Type of interchangeTrainCodes '+typeof interchangeTrainCodes +" "+ interchangeTrainCodes,'Length of traincodes'+ interchangeTrainCodes.length)
+                //   console.log('Type of interchangeTrainCodes '+typeof interchangeTrainCodes +" "+ interchangeTrainCodes,'Length of traincodes'+ interchangeTrainCodes.length)
                 for(let k = 0; k < interchangeTrainCodes.length;k++){
                     //Need to check code
-                    zeros[i][trainCodeList.indexOf(interchangeTrainCodes[k]) ] =  8
-                    console.log(`Added interchange ${interchangeTrainCodes[k]}`)
-                    console.log(`Added interchange ${trainCodeList.indexOf(interchangeTrainCodes[k])}`)
+                    zeros[i][trainCodeList.indexOf(interchangeTrainCodes[k]) ] =  10
+                    // console.log(`Added interchange ${interchangeTrainCodes[k]}`)
+                    // console.log(`Added interchange ${trainCodeList.indexOf(interchangeTrainCodes[k])}`)
                 }
                 // To assign value of interchange wait time
             }
@@ -97,14 +95,10 @@ function readCSV(){
 
 }
 
-// A utility function to find the
-// vertex with minimum distance
-// value, from the set of vertices
-// not yet included in shortest
-// path tree
+// A utility function to find the vertex with minimum distance value, from the set of vertices
+// not yet included in shortest path tree
 function minDistance(dist,sptSet)
 {
-	
 	// Initialize min value
 	let min = Number.MAX_VALUE;
 	let min_index = -1;
@@ -122,16 +116,27 @@ function minDistance(dist,sptSet)
 
 // A utility function to print
 // the constructed distance array
-function printSolution(dist)
+function printSolution(dist,src,target)
 {
-	console.log("Train \t\t\t\t Distance from Source<br>");
-	for(let i = 0; i < currentVertices; i++)
-	{
-		console.log( i ," ",trainName[i] + " \t\t\t\t " +
-				dist[i]);
-	}
+	console.log("Train \t\t\t\t Distance from Source \t\t\t Path");
+	// for(let i = 0; i < currentVertices; i++)
+	// {
+	// 	console.log( i ," ",trainName[i] + " \t\t\t\t " +
+	// 			dist[i]+"\t\t\t"+printPath(i,parents,array));
+	// }
+    process.stdout.write(trainName[target]+ "\t\t\t\t"+dist[target]+"\t\t\t\t")
+    printPath(target,parents)
+    
+    
+    console.log(`\nTrain from ${trainName[src]} to ${trainName[target]} takes ${dist[target]} mins minimally`)
+}
 
-    console.log(`Train from ${trainName[2]} to ${trainName[116]} takes ${dist[116]} mins minimally`)
+function printPath(currentVertex,parents,){
+    if(currentVertex == -1){
+        return;
+    }
+    printPath(parents[currentVertex],parents)
+    process.stdout.write(trainCodeList[currentVertex] +" ")
 }
 
 // Function that implements Dijkstra's
@@ -153,6 +158,7 @@ function dijkstra(graph, src,target,V)
 	
 	// Distance of source vertex
 	// from itself is always 0
+    parents[src] = -1
 	dist[src] = 0;
 	minDistanceFound = false
 	// Find shortest path for all vertices
@@ -164,7 +170,6 @@ function dijkstra(graph, src,target,V)
 		// processed. u is always equal to
 		// src in first iteration.
 		let u = minDistance(dist, sptSet);
-
 		// Mark the picked vertex as processed
 		sptSet[u] = true;
 		
@@ -178,29 +183,23 @@ function dijkstra(graph, src,target,V)
 			// to v, and total weight of path
 			// from src to v through u is smaller
 			// than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] != 0 &&
-				dist[u] != Number.MAX_VALUE &&
-				dist[u] + graph[u][v] < dist[v])
+			if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Number.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
 			{
+                parents[v] = u
+                //shortestDistances[vertexIndex] = shortestDistance + edgeDistance
 				dist[v] = dist[u] + graph[u][v];
 			}
 		}
 	}
-	
 	// Print the constructed distance array
-	printSolution(dist);
+	printSolution(dist,src,target);
 }
 
 
 async function driver(){
-    var data = await readCSV()
+    await readCSV()
     var matrix =  await createAdjMatrix(166,166)
-    var timeTaken = await dijkstra(matrix,2,116,166)
-    
-    //  .forEach((e,idx) => console.log(`Index ${idx} with element ${e} at ${trainName[idx]} with ${trainCodeList[idx]}`))
-    // console.log(`Currently at train code ${trainName[24]} ${trainCodeList[24]}`)
-
-    //Enter Djkstra Algorithm here
+    var timeTaken = await dijkstra(matrix,79,128,166)
 }
 
 driver()

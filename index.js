6d16f6dae6303096a12851f19a731ce4a90d5166
darrenhke <91587264+darrenhke@users.noreply.github.com>
2022-07-06 11:32:31 +0800

    Added time factor but need to change input argument type and refactor code

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const csv = require('fast-csv');
 
 //TODO: Create Djkstra algorithm and route person without startDateTime factors
 //TODO: Find route from start to end destination
-//TODO: Add in time factor
+//TODO: Change argument to locations instead of traincode
+//TODO: Write conditions to remove the weight if starting from a wrong station
 
 const trainMap = new Map()
 const interMap = new Map()
@@ -13,8 +14,9 @@ const trainName = []
 const parents = new Array(166)
 const pathListIndex = []
 
-let dist = new Array()
+var dist = new Array()
 var currentVertices = 0 
+var timeTaken = 0
 
 function readCSV(){
     return new Promise((resolve)=>{
@@ -48,14 +50,14 @@ function readCSV(){
 
 //TODO: Create addition function argument dateTime to determine weight to use per edge
 // Helper Function to create adjacency matrix
- function createAdjMatrix(rows,columns){
+ function createAdjMatrix(vertices){
     // console.log('Entry into createAdjMatrix func')
     //Create 2D array
-    const graph = Array.from(Array(rows), _ => Array(columns).fill(0));
-    for(let i =0; i < rows; i++){
+    const graph = Array.from(Array(vertices), _ => Array(vertices).fill(0));
+    for(let i =0; i < vertices; i++){
         var currentVerticeTrainCode = trainCodeList[i].slice(0,2) 
         // console.log(i)
-        for(let j =0; j < columns; j++){
+        for(let j =0; j < vertices; j++){
             //Check if there is adjacent train station in the previous and exist
             if(trainCodeList[j-1]){
                 var previousVertice = trainCodeList[j-1]
@@ -114,10 +116,11 @@ function minDistance(dist,sptSet)
 // the constructed distance array
 function printSolution(dist,src,target)
 {
+    timeTaken = dist[target]
 	console.log("Train \t\t\t\t Distance from Source \t\t\t Path");
     process.stdout.write(trainName[target]+ "\t\t\t\t"+dist[target]+"\t\t\t\t")
     getPath(target,parents)
-    console.log(`\nTrain from ${trainName[src]} to ${trainName[target]} takes ${dist[target]} mins minimally`)
+    console.log(`\nTrain from ${trainName[src]} to ${trainName[target]} takes ${timeTaken} mins minimally`)
     console.log("\b")
 }
 
@@ -131,7 +134,7 @@ function getPath(currentVertex,parents,){
 }
 
 function printPath(pathList){
-    orderedPathList = pathList.reverse()
+    let orderedPathList = pathList.reverse()
     let iteration = 0 
     while(iteration != orderedPathList.length-1){
         let currentStation = orderedPathList[iteration]
@@ -152,6 +155,68 @@ function printPath(pathList){
         iteration++
     }   
 }
+
+//Utility function to calculate the time from src to destination station factoring peak/off-peak and night trains
+// function calculatimeTimeTaken(pathList,srcTime,timeTaken){
+//     let orderedPathList = pathList.reverse()
+//     let currentTime = srcTime
+//     let iteration = 0
+//     let noRouteFound = false
+
+//     while(iteration != orderedPathList.length-1 && noRouteFound == false){
+//         let currentStation = orderedPathList[iteration]
+//         let nextStation = orderedPathList[iteration+1]
+//         let currentTrainCodeLetters = trainCodeList[currentStation].slice(0,2)
+//         let currentTrain = trainName[currentStation]
+//         let nextTrainCodeLetters = trainCodeList[nextStation].slice(0,2)
+//         let nextTrain = trainName[nextStation]
+
+//         switch(true){
+//             //Peak Period Case
+//             case currentTime > morningPeakPeriod1 && currentTime < morningPeakPeriod2 || (currentTime > eveningPeakPeriod1 && currentTime < eveningPeakPeriod2):{
+//                 let nonNightStations = ['NS','NE']
+//                                 if(currentTrainCodeLetters.includes(nextTrainCodeLetters) && nonNightStations.includes(currentTrainCodeLetters)){
+//                     timeTaken += 2
+//                     //currentTime += 12
+//                 }
+//                 //Currently going through an interchange
+//                 if(currentTrain.includes(nextTrain)){
+//                     timeTaken += 5
+//                     //currentTime += 15
+//                 }
+//                 break
+//             }
+//             //Night Period Case
+//             case currentTime > nightTimePeriod1 && currentTime < nightTimePeriod2:{
+//                 let nonNightStations = ['CG','DT','CE']
+//                 if(nonNightStations.includes(currentTrainCodeLetters)){
+//                     console.log("No Route found to destination")
+//                     noRouteFound =true 
+//                 }
+//                 if(currentTrainCodeLetters.includes(nextTrainCodeLetters) && currentTrainCodeLetters.includes('TE')){
+//                     timeTaken -= 2
+//                     //     Minus current time for TE lines in non-peak hours
+//                     //     currentTime += 8min
+//                 }
+//                 break
+
+//             }
+//             //Non-Peak Period Case
+//             default:{
+//                 let noPeakTrainCode = ['DT','TE']
+//                 //Check if current train station code is the same as next train code letters. Check also with they are DT or TE line
+//                 if(currentTrainCodeLetters.includes(nextTrainCodeLetters) && noPeakTrainCode.includes(currentTrainCodeLetters)){
+//                     timeTaken -= 2
+//                     //     Minus current time for DT and TE lines in non-peak hours
+//                     //     currentTime += 8
+//                 }
+//                 //Track current time
+//                 //currentTime += 10
+//                 break
+//             }
+//         }
+//     }
+// }
 
 
 // Function that implements Dijkstra's
@@ -203,6 +268,7 @@ async function driver(){
     var matrix =  await createAdjMatrix(166,166)
     var timeTaken = await dijkstra(matrix,79,128,166)
     printPath(pathListIndex)
+    // calculatimeTimeTaken()
 }
 
 driver()
